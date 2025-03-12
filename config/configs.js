137aae5f1012be8d1694c77f000d93d4c8e403b2
Profeci <harshkumar2002jha@gmail.com>
2025-03-12 22:44:16 +0530

    Done

---
+++ config/configs.js
@@ -0,0 +1,7 @@
+module.exports = {
+    database: 'mongodb+srv://harsh:harsh@mern-estate.huxd3yr.mongodb.net/Task_Manager',
+    secret: '$ecreate12345',
+    enableConsoleLog: true,
+    enableFileLog:false,
+    tokenExpire: 36000 //10 hours
+}
